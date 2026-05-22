63054876be8a0742cd4ac61f8928aea6c2d67387
Pratik Mohod <pratikmohod0311@gmail.com>
2026-05-22 21:40:16 +0530

    Update Django deployment settings

--- backend/settings.py
+++ backend/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
@@ -154,6 +154,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "https://ecommerce-frontend-gamma-seven.vercel.app",
+]
+
+CSRF_TRUSTED_ORIGINS = [
     "https://ecommerce-frontend-gamma-seven.vercel.app",
 ]
 
