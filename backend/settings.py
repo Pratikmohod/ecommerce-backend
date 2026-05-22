a011b351dc7c3e633f3936447b797cb4d6a53dbc
Pratik Mohod <pratikmohod0311@gmail.com>
2026-05-22 11:44:24 +0530

    Updated settings.py for Render + Aiven MySQL deployment

--- backend/settings.py
+++ backend/settings.py
@@ -30,7 +30,11 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['your-backend.onrender.com']
+ALLOWED_HOSTS = [
+    'your-backend.onrender.com',
+    'localhost',
+    '127.0.0.1'
+]
 
 
 
@@ -50,9 +54,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -91,7 +95,11 @@ DATABASES = {
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        'PORT': config('DB_PORT', default='3306'),
+        'OPTIONS': {
+            **({'ssl': {'sslmode': 'REQUIRED'}} if config('DB_SSL', default=False, cast=bool) else {})
+        }
+        
     }
 }
 
@@ -132,7 +140,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR/'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
@@ -141,11 +149,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default_Auto_field = 'django.db.models.BigAutoField'
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:5173"
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "https://yourfrontend.vercel.app",
+]
 
-# CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = True
 # CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
